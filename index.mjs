b997b5ce95a46de20e8ccb0ed3352eebae3ce3fb
Masahiro Nagano <m-nagano@sakura.ad.jp>
2021-08-30 16:19:48 +0900

    initial commit

---
+++ index.mjs
@@ -0,0 +1,41 @@
+import express, { json, urlencoded } from "express";
+import safe from 'safe-regex';
+var app = express();
+
+app.use(json())
+app.use(urlencoded({ extended: true }));
+
+var server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+app.post("/is_safe_regexp", function(req, res, next){
+    const regexp = req.body.regexp
+    if (regexp === undefined) {
+        res.statusCode = 400
+        res.json({error: true, message: "No regexp posted"})
+        return
+    }
+    const is_safe = safe(regexp)
+    res.json({error: false, is_safe: is_safe})
+});
+
+app.post("/is_safe_regex", function(req, res, next){
+    const regex = req.body.regex
+    if (regex === undefined) {
+        res.statusCode = 400
+        res.json({error: true, message: "No regex posted"})
+        return
+    }
+    const is_safe = safe(regex)
+    res.json({error: false, is_safe: is_safe})
+});
+
+app.get("/live", function(req, res, next){
+    res.json({status: "OK"})
+});
+
+app.get("/", function(req, res, next){
+    res.statusCode = 403
+    res.json({})
+});
